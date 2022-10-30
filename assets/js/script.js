3baf57cfbe5e80d3f09471cca3a625a9518be93a
TDGLU <tdglukealt@gmail.com>
2022-10-30 01:54:17 -0700

    storing html classes and ids in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 console.log('Hello')
 
+// Search Section 
+
+const searchText = document.getElementById('searchText')
+const searchBtn = document.getElementById('searchBtn')
+
+// History Section
+
+
+
+// API Section 
+
 let weatherapi = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b23df2e93e0f4913efaf4a0404c91c0'
+let APIKey = '7b23df2e93e0f4913efaf4a0404c91c0'
 
 fetch(weatherapi)
   .then((response) => response.json())
