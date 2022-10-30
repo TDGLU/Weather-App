3f2dc59a839b7633de1b7c9deea9987f4bb2f57d
TDGLU <tdglukealt@gmail.com>
2022-10-30 05:40:54 -0700

    added fetch weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,24 @@ const history = document.getElementById('history')
 const historyItem = document.querySelectorAll('.history-item')
 const clearHistoryBtn = document.getElementById('clearHistory')
 
+// Current City Information
+
+const currentCityTemp = document.getElementById('currentCityTemp')
 
 // API Section 
 
-let weatherapi = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b23df2e93e0f4913efaf4a0404c91c0'
 let APIKey = '7b23df2e93e0f4913efaf4a0404c91c0'
+let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=perris&appid=${APIKey}`
 
-fetch(weatherapi)
+console.log(weatherApi)
+
+// Calls the weather API 
+function fetchWeather() {
+  fetch(weatherApi)
   .then((response) => response.json())
   .then((data) => console.log(data))
+}
+
+// fetchWeather()
+
+searchBtn.addEventListener('click', fetchWeather)
